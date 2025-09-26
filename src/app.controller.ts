359e983b10910869105e7ad241b3780a5a8669b6
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-26 19:58:45 +0200

    👨‍💻 use decorator in controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Endpoint } from './decorators/endpoint';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Endpoint('GET', '/', {
+    code: 200,
+  })
   getHello(): string {
     return this.appService.getHello();
   }
