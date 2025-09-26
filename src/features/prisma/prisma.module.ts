da0237526695f267657c4c16e6ba680690635b94
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-26 19:44:15 +0200

    🫗 create global prisma module

---
+++ src/features/prisma/prisma.module.ts
@@ -0,0 +1,9 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Global() // Make this module global, because prisma is used everywhere
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
