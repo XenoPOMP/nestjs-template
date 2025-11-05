bc5b2760670e00fc2deae41b203e830a08e37288
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-11-05 09:54:45 +0200

    🧼 remove env population in endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { EnvironmentService } from './features/environment/environment.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly env: EnvironmentService) {}
-
   getHello() {
     return {
       message: 'Hello World!',
-      isProd: this.env.isProduction(),
     };
   }
 }
