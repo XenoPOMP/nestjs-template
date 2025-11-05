133522e3e05404d525ce135c99f205cad748c183
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-11-05 13:23:52 +0200

    🍪 use cookies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [process.env.APP_HOST],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
+
   await app.listen(process.env.PORT ?? 4242);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
