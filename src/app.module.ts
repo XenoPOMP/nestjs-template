bafc3b1727fc0f22850d2ab7379da45490716031
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-11-05 11:13:07 +0200

    🙆‍♂️ create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvironmentModule } from './features/environment/environment.module';
 import { PrismaModule } from './features/prisma/prisma.module';
+import { UserModule } from './routes/user/user.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot(), EnvironmentModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot(),
+    EnvironmentModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
