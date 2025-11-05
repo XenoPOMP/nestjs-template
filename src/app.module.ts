cd9985efaf24d2e3a85d50439fafa7c976f18091
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-11-05 08:11:45 +0200

    🤫 add zod

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './features/prisma/prisma.module';
+import { EnvironmentModule } from './features/environment/environment.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot()],
+  imports: [PrismaModule, ConfigModule.forRoot(), EnvironmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/features/environment/environment.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { z } from 'zod';
+
+/**
+ * This service utilizes zod and @nestjs/config under the hood
+ * to provide type-safe env object.
+ */
+@Injectable()
+export class EnvironmentService {
+  constructor(private readonly configService: ConfigService) {}
+
+  /**
+   * Convert nest.js config response to Zod parsed schema.
+   * @throws The whole app will be downed if .env file is incorrect.
+   */
+  schema() {
+    return environmentSchema.parse(process.env);
+  }
+}
+
+const environmentSchema = z.object({
+  // For docker compose
+  PGDATABASE: z.string(),
+  PGUSER: z.string(),
+  PGPASSWORD: z.string(),
+
+  // For local Prisma development
+  DATABASE_URL: z.string().optional(),
+
+  // For cookie assignee
+  APP_HOST: z.string(),
+
+  // Other
+  NODE_ENV: z.string().optional(),
+});
+
+export type EnvironmentSchema = z.infer<typeof environmentSchema>;
