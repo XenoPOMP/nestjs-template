cf4ccecb5574316e013e01059b38db9a5b23b789
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-11-05 09:48:17 +0200

    ✍️ change env example

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { EnvironmentService } from './features/environment/environment.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly env: EnvironmentService) {}
+
   getHello() {
     return {
       message: 'Hello World!',
+      isProd: this.env.isProduction(),
     };
   }
 }
