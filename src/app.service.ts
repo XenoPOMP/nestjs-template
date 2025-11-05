564bfc7c37fad295e2cafa40a0106df6b0f6b5ff
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-11-05 08:29:08 +0200

    🧼 return env from app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { EnvironmentService } from './features/environment/environment.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly env: EnvironmentService) {}
-
   getHello() {
-    const { NODE_ENV: env } = this.env.schema();
-
     return {
       message: 'Hello World!',
-      env,
     };
   }
 }
