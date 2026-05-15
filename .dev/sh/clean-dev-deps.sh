cd092b143f9b55e7623a545716f3cb925ea7f5be
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-05-16 00:47:27 +0200

    ✍️ do not deleting typescript

--- .dev/sh/clean-dev-deps.sh
+++ .dev/sh/clean-dev-deps.sh
@@ -3,7 +3,6 @@
 rm -rf $(find ./node_modules/@next -type d -name "swc*" -maxdepth 1 | paste -s -d ' ')
 rm -rf ./node_modules/next
 rm -rf ./node_modules/@img
-rm -rf ./node_modules/typescript
 rm -rf ./node_modules/prettier
 rm -rf ./node_modules/@typescript-eslint
 rm -rf ./node_modules/eslint
