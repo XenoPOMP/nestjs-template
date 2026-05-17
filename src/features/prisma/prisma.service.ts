d814ac70edbbdda1e0b68483ee6f81472b8d6d0b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-05-17 16:50:20 +0200

    🧑‍💻 fix prisma service

--- src/features/prisma/prisma.service.ts
+++ src/features/prisma/prisma.service.ts
@@ -1,11 +1,21 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '~prisma/client';
+
+import { EnvironmentService } from '@/features/environment/environment.service';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor(private readonly env: EnvironmentService) {
+    const adapter = new PrismaPg({
+      connectionString: env.schema.DATABASE_URL,
+    });
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
