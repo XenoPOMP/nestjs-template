ed63fb91f4221c4a03c6d675676eae4646724e36
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-05-24 17:40:14 +0200

    ⬇️ update setup script

--- test/vitest.setup.ts
+++ test/vitest.setup.ts
@@ -2,3 +2,8 @@ import * as dotenv from 'dotenv';
 
 // Automatically parses your .env file into process.env
 dotenv.config({ quiet: true });
+
+// Mock dotenv
+process.env.APP_HOST = 'localhost';
+process.env.JWT_SECRET = 'testing-secret';
+process.env.ARGON_SECRET = 'testing';
