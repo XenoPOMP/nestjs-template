5baef319c2cf453976b40ae9cebc116354066127
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-05-24 15:12:08 +0200

    🧪 create EnvironmentService test

---
+++ src/features/environment/environment.service.spec.ts
@@ -0,0 +1,43 @@
+import type { TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+
+import { EnvironmentModule } from './environment.module';
+import { EnvironmentService } from './environment.service';
+
+describe('EnvironmentService', () => {
+  let env: EnvironmentService;
+  const OLD_ENV = process.env;
+
+  beforeEach(async () => {
+    await parseEnv();
+  });
+
+  afterEach(() => {
+    // Reset env after each test
+    process.env = OLD_ENV;
+  });
+
+  describe('root', () => {
+    it('should calculate production status', () => {
+      const isProd: boolean = env.isProduction();
+      expect(isProd).toBeFalsy();
+    });
+
+    it('should calculate production status (if NODE_ENV=production)', async () => {
+      // Mock NODE_ENV
+      process.env.NODE_ENV = 'production';
+      await parseEnv();
+      // Should be true
+      const isProd: boolean = env.isProduction();
+      expect(isProd).toBeTruthy();
+    });
+  });
+
+  async function parseEnv(): Promise<void> {
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [EnvironmentModule],
+    }).compile();
+
+    env = app.get(EnvironmentService);
+  }
+});
