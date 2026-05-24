f9ca5c4e055c5c5fbaf0060f6b405839213b7677
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-05-24 15:24:20 +0200

    🧪 test dayjs plugins

---
+++ src/features/dayjs/dayjs.spec.ts
@@ -0,0 +1,19 @@
+import dayjs from 'dayjs';
+
+import '../dayjs';
+
+describe('dayjs plugin setup', () => {
+  describe('root', () => {
+    it('should apply plugins', () => {
+      const today = dayjs(Date.now()).startOf('day');
+      expect(today.format('HH:mm:ss')).toStrictEqual('00:00:00');
+      expect(today.format()).toMatch(/T(\d{2}:*){3}\+(\d{2}:*){2}$/);
+    });
+
+    it('should stringify properly', () => {
+      const today = dayjs(Date.now()).startOf('day');
+      const json = today.toJSON();
+      expect(json).toMatch(/^(\d-*)+T(\d{2}:*){3}\+(\d{2}:*){2}$/);
+    });
+  });
+});
