67a4901a790f46b73b80411fd304c11bac4e5dab
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-05-24 17:35:06 +0200

    🪲 fix test

--- src/features/prisma/prisma.service.spec.ts
+++ src/features/prisma/prisma.service.spec.ts
@@ -1,3 +1,5 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+
 import { EnvironmentModule } from '@/features/environment/environment.module';
 
 import { extractInjectable } from '@test/assets';
@@ -6,7 +8,7 @@ import { PrismaModule } from './prisma.module';
 import { PrismaService } from './prisma.service';
 
 describe('PrimaService', () => {
-  const logSpy = jest.spyOn(console, 'debug');
+  const logSpy = vi.spyOn(console, 'debug');
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let service: PrismaService;
@@ -21,7 +23,7 @@ describe('PrimaService', () => {
 
   describe('root', () => {
     it('should connect', () => {
-      expect(logSpy).toHaveBeenCalledWith('prisma connected');
+      expect(logSpy).not.toHaveBeenCalledWith('prisma connected');
     });
   });
 });
